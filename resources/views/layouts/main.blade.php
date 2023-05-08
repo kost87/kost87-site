d568b2add6bdf628b2aee56d117e22ab21d40f53
kost <kost_87@list.ru>
2023-05-08 23:45:13 +0300

    Removed footer at main view

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Edica :: Home</title>
+    <title>Kost87</title>
     <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/font-awesome/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/aos/aos.css') }}">
@@ -44,8 +44,8 @@
                             @endauth
                         </li>
 
-
-                        <!-- <li class="nav-item">
+                        <!-- 
+                       <li class="nav-item">
                             <a class="nav-link" href="about.html">About</a>
                         </li>
                         <li class="nav-item dropdown">
@@ -55,6 +55,7 @@
                                 <a class="dropdown-item" href="blog-single.html">Blog Post</a>
                             </div>
                         </li>
+                        
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
                             <div class="dropdown-menu" aria-labelledby="pagesDropdown">
@@ -65,16 +66,16 @@
                         <li class="nav-item">
                             <a class="nav-link" href="contact.html">Contact</a>
                         </li>
+                        </ul>
+                        <ul class="navbar-nav mt-2 mt-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><span class="flag-icon flag-icon-squared rounded-circle flag-icon-gb"></span> Eng</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Download</a>
+                            </li>
+                        </ul> -->
                     </ul>
-                    <ul class="navbar-nav mt-2 mt-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><span class="flag-icon flag-icon-squared rounded-circle flag-icon-gb"></span> Eng</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Download</a>
-                        </li>
-                    </ul> -->
-
                 </div>
             </nav>
         </div>
@@ -82,80 +83,8 @@
 
     @yield('content')
 
-    <section class="edica-footer-banner-section">
-        <div class="container">
-            <div class="footer-banner" data-aos="fade-up">
-                <h1 class="banner-title">Download it now.</h1>
-                <div class="banner-btns-wrapper">
-                    <button class="btn btn-success"> <img src="{{ asset('assets/images/apple@1x.svg') }}" alt="ios" class="mr-2"> App Store</button>
-                    <button class="btn btn-success"> <img src="{{ asset('assets/images/android@1x.svg') }}" alt="android" class="mr-2"> Google Play</button>
-                </div>
-                <p class="banner-text">Add some helper text here to explain the finer details of your <br> product or service.</p>
-            </div>
-        </div>
-    </section>
-    <footer class="edica-footer" data-aos="fade-up">
-        <div class="container">
-            <div class="row footer-widget-area">
-                <div class="col-md-3">
-                    <a href="index.html" class="footer-brand-wrapper">
-                        <img src="{{ asset('assets/images/logo.svg') }}" alt="edica logo">
-                    </a>
-                    <p class="contact-details">hello@edica.com</p>
-                    <p class="contact-details">+23 3000 000 00</p>
-                    <nav class="footer-social-links">
-                        <a href="#!"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#!"><i class="fab fa-twitter"></i></a>
-                        <a href="#!"><i class="fab fa-behance"></i></a>
-                        <a href="#!"><i class="fab fa-dribbble"></i></a>
-                    </nav>
-                </div>
-                <div class="col-md-3">
-                    <nav class="footer-nav">
-                        <a href="#!" class="nav-link">Company</a>
-                        <a href="#!" class="nav-link">Android App</a>
-                        <a href="#!" class="nav-link">ios App</a>
-                        <a href="#!" class="nav-link">Blog</a>
-                        <a href="#!" class="nav-link">Partners</a>
-                        <a href="#!" class="nav-link">Careers</a>
-                    </nav>
-                </div>
-                <div class="col-md-3">
-                    <nav class="footer-nav">
-                        <a href="#!" class="nav-link">FAQ</a>
-                        <a href="#!" class="nav-link">Reporting</a>
-                        <a href="#!" class="nav-link">Block Storage</a>
-                        <a href="#!" class="nav-link">Tools & Integrations</a>
-                        <a href="#!" class="nav-link">API</a>
-                        <a href="#!" class="nav-link">Pricing</a>
-                    </nav>
-                </div>
-                <div class="col-md-3">
-                    <div class="dropdown footer-country-dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="footerCountryDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="flag-icon flag-icon-gb flag-icon-squared"></span> United Kingdom <i class="fas fa-chevron-down ml-2"></i>
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="footerCountryDropdown">
-                            <button class="dropdown-item" href="#">
-                                <span class="flag-icon flag-icon-us flag-icon-squared"></span> United States
-                            </button>
-                            <button class="dropdown-item" href="#">
-                                <span class="flag-icon flag-icon-au flag-icon-squared"></span> Australia
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom-content">
-                <nav class="nav footer-bottom-nav">
-                    <a href="#!">Privacy & Policy</a>
-                    <a href="#!">Terms</a>
-                    <a href="#!">Site Map</a>
-                </nav>
-                <p class="mb-0">© Edica. 2020 <a href="https://www.bootstrapdash.com" target="_blank" rel="noopener noreferrer" class="text-reset">bootstrapdash</a> . All rights reserved.</p>
-            </div>
-        </div>
-    </footer>
+    <br>
+    <br>
     <script src="{{ asset('assets/vendors/popper.js/popper.min.js') }}"></script>
     <script src="{{ asset('assets/vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('assets/vendors/aos/aos.js') }}"></script>
